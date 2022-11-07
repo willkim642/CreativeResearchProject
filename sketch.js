9c9b51bd9addecd9cb012c5d775b0c0edc28c414
willkim642 <111807821+willkim642@users.noreply.github.com>
2022-11-07 11:57:04 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,10 @@ function preload() {
   texts[1] = 'You have fully commited to the quarantine lifestyle. You avoid all possible contact with the outside world.';
   texts[2] = 'You work remotely and have thus far lived off of a surplus of supplies hoarded at the beginning of the pandemic.';
   texts[3] = 'However after three months, you have finally depleted the last of your resources. For the first time in a quarter of a year, you MUST leave your home.';
-  texts[4] = 'It is time to begin your journey';
-  texts[5] = 'Day 1: The First Day Out';
+  texts[4] = "You have not met another person face to face since you've holed yourself up in your house. Do you even remember how to talk to people?";
+  texts[5] = "You're dreading the feeling of having to talk to someone out loud again but you have to stock your shelves.";
+  texts[6] = "In spite of your anxieties, you get up from your bed and begin to head out.";
+  texts[7] = 'Day 1: The First Day Out';
 }
 
 function setup() {
@@ -27,7 +29,7 @@ function keyPressed() {
   textNum += 1;
   text(texts[textNum], 0, 190, 640,480);
   
-  if (textNum >= 5) {
+  if (textNum >= 7) {
     textSize(18);
     text('Hit the button to advance', 335, 450, 400, 100);
   }
