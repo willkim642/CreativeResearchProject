b649758c503fe9570a47d7620c925662e9068fad
willkim642 <111807821+willkim642@users.noreply.github.com>
2022-11-06 23:09:16 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,34 @@
-//player variables
-x = 50;
-y = 400;
+let texts = [];
+let textNum = 0;
+
+function preload() {
+  texts[0] = 'In early 2020, following the emergence of the Covid-19 pandemic. The world enters a state of lockdown';
+  texts[1] = 'You have fully commited to the quarantine lifestyle. You avoid all possible contact with the outside world.';
+  texts[2] = 'You work remotely and have thus far lived off of a surplus of supplies hoarded at the beginning of the pandemic.';
+  texts[3] = 'However after three months, you have finally depleted the last of your resources. For the first time in a quarter of a year, you MUST leave your home.';
+  texts[4] = 'It is time to begin your journey';
+  texts[5] = 'Day 1: The First Day Out';
+}
 
 function setup() {
-    createCanvas(700, 500);
+  createCanvas(640, 480);
+  background(0);
+  textAlign(CENTER);
+  noStroke();
+  fill(250);
+  textSize(36);
+  textFont('Franklin Gothic Medium MS');
+  text(texts[textNum], 0, 190, 640,480);
 }
 
-function draw() {
-    background(255);
-    text('p5.js sketch here', width / 2, height / 2);
-    textAlign(CENTER);
-    playerActions();
-    push();
-    rect(x, y, 50);
-    pop();
-}
-
-function playerActions() {
-    if (keyIsDown(LEFT_ARROW)) {
-        x -= 2.5;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        x += 2.5;
-    }
+function keyPressed() {
+  clear();
+  background(0);
+  textNum += 1;
+  text(texts[textNum], 0, 190, 640,480);
+  
+  if (textNum >= 5) {
+    textSize(18);
+    text('Hit the button to advance', 335, 450, 400, 100);
+  }
 }
